e91c436ae2270145f94ac9a708f4f5e5ca9c4091
Mihai1481 <mihaieduard1481@gmail.com>
2024-03-03 16:23:51 +0200

    Show the buttons on display

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const screen = document.querySelector(".currentScreen");
+
+let a;
+let b;
+let op;
+
+const buttons = document.querySelectorAll('.inputButton');
+buttons.forEach(element => element.addEventListener('click', function () { showOnScreen(element) }));
+
+function showOnScreen(button) {
+    screen.textContent += button.textContent;
+}
+
+function operate(op, a, b) {
+    if (op === '+') {
+        add(a, b);
+    } else if (op === '-') {
+        subtract(a, b);
+    } else if (op === '*') {
+        multiply(a, b);
+    } else if (op === '/') {
+        divide(a, b);
+    }
+}
 function add(a, b) {
     return a + b;
 }
@@ -7,38 +31,3 @@ function subtract(a, b) {
 function multiply(a, b) {
     return a * b;
 }
-function divide(a, b) {
-    if (b === 0) {
-        return null;
-    }
-    return a / b;
-}
-
-
-// console.log(add(1,2));
-// console.log(add(50,30));
-// console.log(substract(1,2));
-// console.log(substract(10,2));
-// console.log(multiply(1,2));
-// console.log(multiply(31,2));
-// console.log(divide(1,2));
-// console.log(divide(1,0));
-// console.log(divide(10,2));
-
-
-function operate(op , a , b){
-    if ( op === '+'){
-       return add(a,b);
-    } else if ( op === '-'){
-       return subtract(a,b);
-    } else if ( op === '*'){
-       return  multiply(a,b);
-    } else if (op === '/') {
-       return divide(a,b);
-    }
-}   
-
-// console.log(operate('+',2,2));
-// console.log(operate('-',1,2));
-// console.log(operate('*',3,2));
-// console.log(operate('/',10,2));
